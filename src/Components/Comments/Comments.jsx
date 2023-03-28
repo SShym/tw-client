292157120179ac7ea07d7424d53a5c9a9d1b0af9
SShym <serh.shym@gmail.com>
2023-03-29 00:54:35 +0300

    fix bug

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,6 +1,6 @@
 import './Comments.css';
 import Avatar from '@mui/material/Avatar';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { addCommentToProduct } from '../../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -9,10 +9,15 @@ import Backdrop from '@mui/material/Backdrop';
 const Comments = ({ comments, setOpenComments, openComments }) => {
     const [text, setText] = useState('');
 
-    const dispatch = useDispatch();
+    const myRef = useRef(null);
+    const dispatch = useDispatch();    
     const { id } = useParams();
 
     const user = useSelector(state => state.authReducer.user);
+
+    useEffect(() => {
+        myRef.current.scrollIntoView();
+    }, [comments]);
 
     const handleChangeText = (e) => setText(e.target.value)
 
@@ -26,16 +31,17 @@ const Comments = ({ comments, setOpenComments, openComments }) => {
             productId: id
         }
 
-        dispatch(addCommentToProduct(formData, setText))
+        text.trim().length > 0 && dispatch(addCommentToProduct(formData, setText))
     }
 
     return(
         <div className='comments-wrap'>
             <Backdrop
-                sx={{ overflowY:'auto', color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                sx={{ display:'flex', flexDirection:'column', overflowY:'auto', color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                 open={openComments}
                 onClick={(e) => setOpenComments(false)}
             >
+                {!user && <span className='not-logged-in'>Login to write a comment!</span>}
                 <div className='comments-block-wrap' onClick={(e) => e.stopPropagation()}>
                     {comments.length === 0 &&
                         <h1>No comments yet</h1>
@@ -45,14 +51,16 @@ const Comments = ({ comments, setOpenComments, openComments }) => {
                             <div className='comments-avatar-block'>
                                 <Avatar src={comment.authorImage} className='comments-avatar'></Avatar>
                                 <span>{comment.description}</span>
+                                {console.log(comment)}
                             </div>
                         )}
                     </div>
-                    <form onSubmit={handleAddComment} className='comments-create-block'>
-                        <input value={text} type="text" onChange={handleChangeText} placeholder='Enter your text here' />
-                        <button onClick={handleAddComment}>create</button>
-                        <input type="submit" hidden />
-                    </form>
+                        <form onSubmit={handleAddComment} className='comments-create-block'>
+                            <input disabled={!user} value={text} type="text" onChange={handleChangeText} placeholder='Enter your message' />
+                            <button disabled={!user} onClick={handleAddComment}>SEND</button>
+                            <input type="submit" hidden />
+                        </form>
+                    <div ref={myRef} />
                 </div>
             </Backdrop>
         </div>
